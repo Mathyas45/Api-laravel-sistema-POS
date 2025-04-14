d36fa7f06afb7165f9532d3cd748d7ea4ce5e9d8
Mathyas45 <75018052@continental.edu.pe>
2025-04-14 01:45:30 -0500

    Primer commit

---
+++ app/Http/Controllers/Roles/RoleController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Roles;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use App\Http\Controllers\Controller;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $search = request()->query("search");
+        $roles = Role::where("name", "ilike", "%" . $search . "%")
+            ->orderBy("name", "desc")
+            ->paginate(10);
+        return response()->json([
+            "roles" => $roles->map(function ($role) {
+                return [
+                    "id" => $role->id,
+                    "name" => $role->name,
+                    "created_at" => $role->created_at->format("Y/m/d h:i:s"),
+                ];
+            })
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // $exist_role = Role::where("name", $request->name)->first();
+        // if ($exist_role) {
+        //     return response()->json([
+        //         "message" => "El rol ya existe",
+        //     ], 422);
+        // }
+        $request->validate([
+            "name" => "required|string|max:255|unique:roles,name",
+        ]);
+        $role = Role::create([
+            "name" => $request->name,
+            "guard_name" => "api"
+        ]);
+        //enlazar con los permisos que tenga
+        $permissions = $request->permissions;
+        foreach ($permissions as $permission) {
+            $role->givePermissionTo($permission);
+        }
+
+        return response()->json([
+            "message" => 200,
+            "role" => $role,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            "name" => "required|string|max:255|unique:roles,name," . $id,
+        ]);
+        $role = Role::find($id);
+        if (!$role) {
+            return response()->json([
+                "message" => "Rol no encontrado",
+            ], 404);
+        }
+        $role->update([
+            "name" => $request->name,
+        ]);
+        //enlazar con los permisos que tenga
+        $permissions = $request->permissions;
+        $role->syncPermissions($permissions);
+
+
+        return response()->json([
+            "message" => 200,
+            "role" => $role,
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //eliminacion de rol pero eliminacion logico
+        $role = Role::find($id);
+        if (!$role) {
+            return response()->json([
+                "message" => "Rol no encontrado",
+            ], 404);
+        }
+        $role->delete();
+        return response()->json([
+            "message" => 200,
+            "role" => $role,
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Contracts\Role;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Roles\RoleController;
+
+Route::group([
+    'prefix' => 'auth',
+    // 'middleware' => ['auth:api', 'permission:edit articles'],
+], function ($router) {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
+    Route::post('/me', [AuthController::class, 'me'])->middleware('auth:api')->name('me');
+});
+
+Route::group([
+    "middleware" => ["auth:api"]
+], function () {
+    Route::resource('roles', RoleController::class);
+});
